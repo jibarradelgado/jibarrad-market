323d3c41d6feb0bb3f6100c2a41383090cce3d28
Jorge Ibarra <hello@jorgeibarra.com.mx>
2023-06-14 18:56:31 -0600

    Initial commit

---
+++ src/main/java/com/jibarrad/market/JibarradMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jibarrad.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibarradMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibarradMarketApplication.class, args);
+	}
+
+}
